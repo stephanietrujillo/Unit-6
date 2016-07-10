510794c898988b6b9e21ad897d9b3e7295d07c75
stephanietrujillo <trujillo-romero_step@student.smc.edu>
2016-07-10 15:36:57 -0700

    Create zeros.js
    This code can determine the number of zeros within an array.

---
+++ zeros.js
@@ -0,0 +1,8 @@
+var n= [9, 25, 0, 67, 97, 0, 5];
+var numberOfZeros= 0;
+for (var i =0, n= a.length; i <n; i += 1) {
+  if (a [i] === 0) {
+    numberOfZeros +=1;
+    }
+  }
+alert (numberOfZeros); 
